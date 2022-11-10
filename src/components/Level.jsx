75b9a7800b386e85dad2a6ca90c60e8c48e0fdca
jjpickering10 <jjpickering10@live.com>
2022-11-10 15:50:18 +0000

    Set up limbo and axe with rapier physics

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import * as THREE from 'three';
 import { RigidBody } from '@react-three/rapier';
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 
 THREE.ColorManagement.legacyMode = false;
@@ -28,11 +27,14 @@ const Start = ({ position = [0, 0, 0] }) => {
   );
 };
 const Spinner = ({ position = [0, 0, 0] }) => {
+  const [speed, setSpeed] = useState(
+    () => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1)
+  );
   const obstacle = useRef();
   useFrame((state) => {
     const time = state.clock.getElapsedTime();
     const rotation = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(0, time, 0)
+      new THREE.Euler(0, time * speed, 0)
     );
     obstacle.current.setNextKinematicRotation(rotation);
   });
@@ -64,11 +66,93 @@ const Spinner = ({ position = [0, 0, 0] }) => {
   );
 };
 
+const Limbo = ({ position = [0, 0, 0] }) => {
+  const [offset, setOffset] = useState(() => Math.random() * Math.PI * 2);
+  const obstacle = useRef();
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const translate = Math.sin(time + offset) + 1.15;
+    obstacle.current.setNextKinematicTranslation({
+      x: position[0],
+      y: position[1] + translate,
+      z: position[2],
+    });
+  });
+  return (
+    <group position={position}>
+      <mesh
+        position={[0, -0.1, 0]}
+        receiveShadow
+        geometry={boxGeometry}
+        material={floorTwoMaterial}
+        scale={[4, 0.2, 4]}
+      />
+      <RigidBody
+        ref={obstacle}
+        type='kinematicPosition'
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+};
+
+const Axe = ({ position = [0, 0, 0] }) => {
+  const [offset, setOffset] = useState(() => Math.random() * Math.PI * 2);
+  const obstacle = useRef();
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const translate = Math.sin(time + offset);
+    obstacle.current.setNextKinematicTranslation({
+      x: position[0] + translate,
+      y: position[1] + 0.75,
+      z: position[2],
+    });
+  });
+  return (
+    <group position={position}>
+      <mesh
+        position={[0, -0.1, 0]}
+        receiveShadow
+        geometry={boxGeometry}
+        material={floorTwoMaterial}
+        scale={[4, 0.2, 4]}
+      />
+      <RigidBody
+        ref={obstacle}
+        type='kinematicPosition'
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          scale={[1.5, 1.5, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+};
+
 const Level = () => {
   return (
     <>
-      <Start position={[0, 0, 4]} />
-      <Spinner />
+      <Start position={[0, 0, 12]} />
+      <Spinner position={[0, 0, 8]} />
+      <Limbo position={[0, 0, 4]} />
+      <Axe position={[0, 0, 0]} />
     </>
   );
 };
